c728a842ad2c5745f99228ef746a670f10d707ef
jrenziads <128422655+jrenziads@users.noreply.github.com>
2024-08-05 21:08:01 -0300

    cambie a funciones

--- web_autos/views.py
+++ web_autos/views.py
@@ -1,50 +1,46 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView, ListView, FormView
 from .forms import BrandForm, CarForm, OwnerForm, SearchForm
 from .models import Brand, Car, Owner
 
-def index(request):
-    cars = Car.objects.all()
-    return render(request, 'web_autos/index.html', {'cars': cars})
+class IndexView(ListView):
+    model = Car
+    template_name = 'web_autos/index.html'
+    context_object_name = 'cars'
 
-def add_brand(request):
-    if request.method == 'POST':
-        form = BrandForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('index')
-    else:
-        form = BrandForm()
-    return render(request, 'web_autos/add_brand.html', {'form': form})
+class AddBrandView(CreateView):
+    model = Brand
+    form_class = BrandForm
+    template_name = 'web_autos/add_brand.html'
+    success_url = reverse_lazy('index')
 
-def add_car(request):
-    if request.method == 'POST':
-        form = CarForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('index')
-    else:
-        form = CarForm()
-    return render(request, 'web_autos/add_car.html', {'form': form})
+class AddCarView(CreateView):
+    model = Car
+    form_class = CarForm
+    template_name = 'web_autos/add_car.html'
+    success_url = reverse_lazy('index')
 
-def add_owner(request):
-    if request.method == 'POST':
-        form = OwnerForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('index')
-    else:
-        form = OwnerForm()
-    return render(request, 'web_autos/add_owner.html', {'form': form})
+class AddOwnerView(CreateView):
+    model = Owner
+    form_class = OwnerForm
+    template_name = 'web_autos/add_owner.html'
+    success_url = reverse_lazy('index')
 
+class SearchView(FormView):
+    form_class = SearchForm
+    template_name = 'web_autos/search.html'
 
-def search(request):
-    if request.method == 'GET':
-        form = SearchForm(request.GET)
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Car.objects.filter(model__icontains=query) 
-            return render(request, 'web_autos/search.html', {'form': form, 'results': results})
-    else:
-        form = SearchForm()
-    return render(request, 'web_autos/search.html', {'form': form})
+            results = Car.objects.filter(model__icontains=query)
+            return self.render_to_response(self.get_context_data(form=form, results=results))
+        return self.render_to_response(self.get_context_data(form=form))
+
+    def form_valid(self, form):
+        query = form.cleaned_data['query']
+        results = Car.objects.filter(model__icontains=query)
+        return self.render_to_response(self.get_context_data(form=form, results=results))
 
